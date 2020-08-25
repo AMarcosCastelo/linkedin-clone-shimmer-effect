3caadf7d1a4f658d1a8386024dffc9c755ff57d1
AMarcosCastelo <marcos-12marcos@hotmail.com>
2020-08-25 15:12:59 -0300

    Updates

---
+++ precache-manifest.e0e90bf230c22ad7c5352b0761461672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795fa93b12053e96ca76803d955fc052",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fccf6fb7aaf40781ef0",
+    "url": "/static/js/2.5a103393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a103393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f808a73fb7d71fe95e73",
+    "url": "/static/js/main.1e01b2d0.chunk.js"
+  },
+  {
+    "revision": "05ab4294c1701456a464",
+    "url": "/static/js/runtime-main.a72213ab.js"
+  }
+]);
